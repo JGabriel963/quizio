88bcf90d6ac46ff358a48a674811084b4a2b282c
João Gabriel <joaogabriel9633@gmail.com>
2026-09-08 22:03:11 -0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { createDb } from "@quizio/db";
+import * as schema from "@quizio/db/schema/auth";
+import { env } from "@quizio/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.BETTER_AUTH_URL],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [tanstackStartCookies()],
+  });
+}
+
+export const auth = createAuth();
